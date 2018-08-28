3bd518d96ec244c3823d0ad4ac0b7591b1660ed2
Josh Hubers <jhubers@oaisd.org>
2018-08-28 12:20:32 -0400

    thread work

--- backend/models/thread.js
+++ backend/models/thread.js
@@ -1,3 +1,4 @@
+import { Project } from './project'
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Thread = sequelize.define('Thread', {
@@ -8,9 +9,11 @@ module.exports = (sequelize, DataTypes) => {
         model: Project,
         key: 'id',
       }
-  }, {});
+    }
+  });
   Thread.associate = function(models) {
     // associations can be defined here
+    Thread.belongsTo(models.Project)
   };
   return Thread;
 };
---
+++ backend/seeders/20180828161347-demo-threads.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    let threads = [];
+    var i;
+    for(i = 0; i < 7*3; i++) {
+      const t = {
+        title: `Test Thread ${i}`,
+        project_id: 1,
+        createdAt: '2018-1-1',
+        updatedAt: '2018-1-1',
+      };
+
+      threads.push(t);
+    }
+
+    return queryInterface.bulkInsert('Threads', threads, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+    return queryInterface.bulkDelete('Threads', null, {});
+  }
+};
